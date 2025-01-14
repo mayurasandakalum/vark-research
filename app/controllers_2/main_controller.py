b1c701caa995cd866b4504d9e02fd8363b9db143
mayurasandakalum <mssellapperuma@gmail.com~>
2025-01-14 18:52:57 +0530

    remove unused KinestheticController and its associated render logic

--- app/controllers_2/main_controller.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-
-
-class KinestheticController:
-    def show_kinesthetic_page(self):
-        return render_template("components/kinesthetic/templates/kinesthetic.html")
