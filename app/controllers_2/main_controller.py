e5e8d939c8b01bc5ecbcbd9f6a2c398803755989
mayurasandakalum <mssellapperuma@gmail.com~>
2025-01-14 18:39:47 +0530

    implement kinesthetic component with routing and template rendering

--- app/controllers_2/main_controller.py
+++ app/controllers_2/main_controller.py
@@ -3,5 +3,4 @@ from flask import render_template
 
 class KinestheticController:
     def show_kinesthetic_page(self):
-        print("Kinesthetic page")
-        return render_template("kinesthetic/index.html")
+        return render_template("components/kinesthetic/templates/kinesthetic.html")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from flask import render_template
 from app import app
+from app.components.kinesthetic.controller import KinestheticController
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/kinesthetic")
+def kinesthetic():
+    controller = KinestheticController()
+    return controller.show_kinesthetic_page()
